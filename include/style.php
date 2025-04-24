4ab9441a8aabc7bf1de96c023ec4db48b69abb59
frostkom <martin.fors@frostkom.se>
2025-04-24 13:47:07 +0200

    Make sure that overlay is able to click through

--- include/style.php
+++ include/style.php
@@ -157,6 +157,7 @@ echo "@media all
 		background: rgba(0, 0, 0, .5);
 		content: '';
 		opacity: 0;
+		pointer-events: none;
 		position: absolute;
 		top: 0; left: 0; right: 0; bottom: 0;
 		transition: opacity 1s;
